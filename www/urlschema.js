ab4e77cd2476cfcf8d3ab311157d4765e60ae975
Dark <Dark@example.com>
2023-03-11 06:58:46 +0300

    update

--- www/urlschema.js
+++ www/urlschema.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'urlschema', 'coolMethod', [arg0]);
 };
+
+exports.test = function (arg0, success, error) {
+    return "Test";
+};
